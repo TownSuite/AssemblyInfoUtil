6cd7674cc5809f112dee86e2e19c91aba424e568
Peter Gill <peter.gill@townsuite.com>
2026-04-09 14:39:41 -0230

    Update build_windows.ps1

--- build_windows.ps1
+++ build_windows.ps1
@@ -6,4 +6,4 @@ New-Item -Path "$CURRENTPATH\build" -ItemType Directory -Force
 dotnet build src\AssemblyInfoUtils.sln -c Release
 
 # net10.0 windows self-contained, single file, aot, build output for distribution
-dotnet publish src\TownSuite.AssemblyInfoUtil.csproj -c Release -r "win-x64" --self-contained true -p:PublishSingleFile=true -p:PublishAot=true -f net10.0 -p:StripSymbols=true -p:DebugType=None -p:CopyOutputSymbolsToPublishDirectory=false -p:DebugType=None -o "$CURRENTPATH\build\TownSuite.AssemblyInfoUtils.Windows"
+dotnet publish src\TownSuite.AssemblyInfoUtil.csproj -c Release -r "win-x64" --self-contained true -p:PublishSingleFile=true -p:PublishAot=true -f net10.0-windows -p:StripSymbols=true -p:DebugType=None -p:CopyOutputSymbolsToPublishDirectory=false -p:DebugType=None -o "$CURRENTPATH\build\TownSuite.AssemblyInfoUtils.Windows"
