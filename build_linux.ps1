da3b4045350ebe345266a7a6c2e8a52285c92d07
Peter Gill <peter.gill@townsuite.com>
2026-06-01 13:04:08 -0230

    Update build scripts and csproj to support net48 and simplify publish options

--- build_linux.ps1
+++ build_linux.ps1
@@ -6,5 +6,5 @@ New-Item -Path "$CURRENTPATH/build" -ItemType Directory -Force
 dotnet build src/AssemblyInfoUtils.sln -c Release
 
 # net10.0 linux self-contained, single file, aot build output for distribution
-dotnet publish src/TownSuite.AssemblyInfoUtil.csproj -c Release -r "linux-x64" --self-contained true -p:PublishSingleFile=true -p:PublishAot=true -f net10.0 -p:StripSymbols=true -p:DebugType=None -p:CopyOutputSymbolsToPublishDirectory=false -p:DebugType=None -o "$CURRENTPATH/build/TownSuite.AssemblyInfoUtils.Linux"    
+dotnet publish src/TownSuite.AssemblyInfoUtil.csproj -c Release -r "linux-x64" --self-contained true -p:PublishSingleFile=true -f net10.0 -o "$CURRENTPATH/build/TownSuite.AssemblyInfoUtils.Linux"    
 
--- build_windows.ps1
+++ build_windows.ps1
@@ -6,4 +6,4 @@ New-Item -Path "$CURRENTPATH\build" -ItemType Directory -Force
 dotnet build src\AssemblyInfoUtils.sln -c Release
 
 # net10.0 self-contained Windows distribution, single file and AOT-enabled
-dotnet publish src\TownSuite.AssemblyInfoUtil.csproj -c Release -r "win-x64" --self-contained true -p:PublishSingleFile=true -p:PublishAot=true -f net10.0 -p:StripSymbols=true -p:DebugType=None -p:CopyOutputSymbolsToPublishDirectory=false -o "$CURRENTPATH\build\TownSuite.AssemblyInfoUtils.Windows"
+dotnet publish src\TownSuite.AssemblyInfoUtil.csproj -c Release -r "win-x64" --self-contained true -p:PublishSingleFile=true -f net10.0 -o "$CURRENTPATH\build\TownSuite.AssemblyInfoUtils.Windows"
