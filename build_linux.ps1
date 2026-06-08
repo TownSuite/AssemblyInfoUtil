2b9eb61ffb0a2fa94c3aeff7bf48791533d671a0
Peter Gill <peter.gill@townsuite.com>
2026-06-08 15:57:50 -0230

    Enhance build scripts to support multi-architecture publishing for Linux and update .NET version to 10.0

--- build_linux.ps1
+++ build_linux.ps1
@@ -6,5 +6,6 @@ New-Item -Path "$CURRENTPATH/build" -ItemType Directory -Force
 dotnet build src/AssemblyInfoUtils.sln -c Release
 
 # net10.0 linux self-contained, single file, aot build output for distribution
-dotnet publish src/TownSuite.AssemblyInfoUtil.csproj -c Release -r "linux-x64" --self-contained true -p:PublishSingleFile=true -f net10.0 -o "$CURRENTPATH/build/TownSuite.AssemblyInfoUtils.Linux"    
+dotnet publish src/TownSuite.AssemblyInfoUtil.csproj -c Release -r "linux-x64" --self-contained true -p:PublishSingleFile=true -f net10.0 -o "$CURRENTPATH/build/TownSuite.AssemblyInfoUtils.Linux.x64"
+dotnet publish src/TownSuite.AssemblyInfoUtil.csproj -c Release -r "linux-arm64" --self-contained true -p:PublishSingleFile=true -f net10.0 -o "$CURRENTPATH/build/TownSuite.AssemblyInfoUtils.Linux.arm64"
 
